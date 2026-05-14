535832c797135975674fc8750ad570e58f973700
bog02ash <bog02ash@mail.ru>
2026-05-14 13:34:09 +0400

    [AB-12] fix error descriptions

--- internal/crypto/crypto.go
+++ internal/crypto/crypto.go
@@ -11,19 +11,19 @@ import (
 
 func Encrypt(plaintext string, key []byte) (ciphertext []byte, err error) {
 	if len(key) != 32 {
-		return nil, errors.New("ключ должен быть длинною 32 байт")
+		return nil, errors.New("the key must be 32 bytes long")
 	}
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка создания блочного шифра: %w", err)
+		return nil, fmt.Errorf("block cipher creation error: %w", err)
 	}
 	gmc, err := cipher.NewGCM(block)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка создания gcm обертки: %w", err)
+		return nil, fmt.Errorf("error creating gcm wrapper: %w", err)
 	}
 	nonce := make([]byte, gmc.NonceSize())
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return nil, fmt.Errorf("ошибка заполнения буфера случаными числами: %w", err)
+		return nil, fmt.Errorf("error filling buffer with random numbers: %w", err)
 	}
 	plaintextbyte := []byte(plaintext)
 	ciphertext = gmc.Seal(nonce, nonce, plaintextbyte, nil)
@@ -31,24 +31,24 @@ func Encrypt(plaintext string, key []byte) (ciphertext []byte, err error) {
 }
 func Decrypt(ciphertext []byte, key []byte) (plaintext string, err error) {
 	if len(key) != 32 {
-		return "", errors.New("ключ должен быть длинною в 32 байт")
+		return "", errors.New("the key must be 32 bytes long")
 	}
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", fmt.Errorf("ошибка создания блочного шифра: %w", err)
+		return "", fmt.Errorf("block cipher creation error: %w", err)
 	}
 	gmc, err := cipher.NewGCM(block)
 	if err != nil {
-		return "", fmt.Errorf("ошибка создания gcm обертки: %w", err)
+		return "", fmt.Errorf("error creating gcm wrapper: %w", err)
 	}
 	nonseSize := gmc.NonceSize()
 	if len(ciphertext) < nonseSize {
-		return "", errors.New("данные слишком короткие")
+		return "", errors.New("data is too short")
 	}
 	nonse, ciphertextWithTag := ciphertext[:nonseSize], ciphertext[nonseSize:]
 	plaintextbyte, err := gmc.Open(nil, nonse, ciphertextWithTag, nil)
 	if err != nil {
-		return "", fmt.Errorf("ошибка при расшифровки данных: %w", err)
+		return "", fmt.Errorf("error while decrypting data: %w", err)
 	}
 	plaintext = string(plaintextbyte)
 	return plaintext, nil
